badfc5e4c1485a8499919dc01a3389bf574f812b
taipham2000 <taitinpro@gmail.com>
2021-03-18 13:39:23 +0700

    1st uploadgit add *!

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author phamv
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("1st upload from local to GitHub!!");
+        System.out.println("Welcome to GitHub");
+    }
+    
+}
